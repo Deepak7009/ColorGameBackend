db98aabe90d9c4446afe698bc08f06be6888e65b
vinunarwal <vinunarwal3@gmail.com>
2024-05-01 10:53:47 -0500

    ..

--- models/transactionSchema .js
+++ models/transactionSchema .js
@@ -4,9 +4,17 @@ const transactionSchema = new mongoose.Schema({
     transactionId: String,
     platform: String,
     amount: Number,
-    userId: String, 
-    status: { type: String, enum: ['pending', 'success', 'failed'], default: 'pending' },
-    timestamp: { type: Date, default: Date.now },
+    userId: String,
+    status: {
+        type: String,
+        enum: ['pending', 'success', 'failed'],
+        default: 'pending'
+    },
+    timestamp:
+    {
+        type: Date,
+        default: Date.now
+    },
 });
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
