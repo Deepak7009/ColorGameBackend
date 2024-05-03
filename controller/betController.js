015a917264545c28cedc95f5f5c3b05c16b3b089
vinunarwal <vinunarwal3@gmail.com>
2024-05-03 12:50:43 -0500

    ..

--- controller/betController.js
+++ controller/betController.js
@@ -4,84 +4,102 @@ const PeriodInfo = require("../models/periodInfoSchema ");
 
 totalAmount = 0;
 let numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let ream = 0;
-let gram = 0;
-let vam = 0;
+let redgiveamount = 0;
+let greengiveamount = 0;
+let purplegiveamount = 0;
+
+const callprice = async (userId, amount, selection, periodId) => {
+  let winAmount = amount;
+  let winAmount2 = amount;
+  let givenAmount = 0;
+  let givenAmount2 = 0;
+
+  if (selection === "Green") {
+    winAmount *= 2; // Multiply by 2 if selection is 'Green'
+    winAmount2 *= 1.5;
+    givenAmount = winAmount; // Set givenAmount to winAmount for 'Green'
+    givenAmount2 = winAmount2; // Set givenAmount to winAmount for 'Green'
+    greengiveamount = greengiveamount + winAmount;
+    totalAmount = greengiveamount;
+
+    numbers[0] += givenAmount2;
+    numbers[1] += givenAmount;
+    numbers[3] += givenAmount;
+    numbers[7] += givenAmount;
+    numbers[9] += givenAmount;
+    console.log("this is the numbers array " + numbers);
+  } else if (selection === "Red") {
+    winAmount *= 2; // Multiply by 2 if selection is 'red'
+    winAmount2 *= 1.5;
+    givenAmount = winAmount; // Set givenAmount to winAmount for 'red'
+    givenAmount2 = winAmount2; // Set givenAmount to winAmount for 'red'
+    redgiveamount = redgiveamount + winAmount;
+    totalAmount = redgiveamount;
+
+    numbers[5] += winAmount2;
+    numbers[2] += winAmount;
+    numbers[4] += winAmount;
+    numbers[6] += winAmount;
+    numbers[8] += winAmount;
+    console.log("this is the numbers array " + numbers);
+  } else if (selection === "Violet") {
+    winAmount *= 4.5; // Multiply by 4.5 if selection is 'Violet'
+    givenAmount = winAmount;
+    purplegiveamount = purplegiveamount + givenAmount;
+    totalAmount = purplegiveamount;
+
+    numbers[0] += winAmount;
+    numbers[5] += winAmount;
+    console.log("this is the numbers array " + numbers);
+  } else if ([1, 3, 7, 9, 2, 4, 6, , 0, 5, 8].includes(parseInt(selection))) {
+    winAmount *= 9; // Multiply by 9 for specific numbers
+    numbers[selection] += winAmount;
+    totalAmount = numbers[selection];
+    console.log("this is the numbers array " + numbers);
+  }
+
+  return winAmount;
+};
 
 const addBet = async (req, res) => {
+   const { userId, amount, selection, periodId } = req.body;
+   let outcome = 'Pending';
    try {
-
-      const { userId, amount, selection, periodId } = req.body;
-
-      let winAmount = amount;
-      let outcome = 'Pending';
-      let givenAmount = 0;
-
-      if (selection === "Green") {
-         winAmount *= 2; // Multiply by 2 if selection is 'Green'
-         givenAmount = winAmount; // Set givenAmount to winAmount for 'Green'
-         gram = gram + winAmount;
-         totalAmount = gram;
-
-         numbers[1] += givenAmount;
-         numbers[3] += givenAmount;
-         numbers[7] += givenAmount;
-         numbers[9] += givenAmount;
-         console.log("this is the numbers array " + numbers);
-      } else if (selection === "Red") {
-         winAmount *= 2; // Multiply by 2 if selection is 'Red'
-         givenAmount = winAmount;
-         ream = ream + winAmount;
-         totalAmount = ream;
-
-         numbers[2] += winAmount;
-         numbers[4] += winAmount;
-         numbers[6] += winAmount;
-         numbers[8] += winAmount;
-         console.log("this is the numbers array " + numbers);
-      } else if ([1, 3, 7, 9, 2, 4, 6, 8].includes(parseInt(selection))) {
-         winAmount *= 9; // Multiply by 9 for specific numbers
-         numbers[selection] += winAmount;
-         totalAmount = numbers[selection];
-         console.log("this is the numbers array " + numbers);
-      } else {
-         winAmount *= 1.4;
-         givenAmount = winAmount;
-         vam = vam + winAmount;
-         totalAmount = vam;
-
-         numbers[0] += givenAmount;
-         numbers[5] += givenAmount;
-         console.log("this is the numbers array " + numbers);
-
-      }
-
-      const bet = new Bet({
-         userId,
-         amount,
-         winAmount,
-         totalAmount,
-         selection,
-         periodId,
-         outcome
-      });
-
-
-      const user = await User.findById(userId);
-      if (!user) {
-         return res.status(404).json({ error: "User not found" });
-      }
-      user.bankBalance -= amount;
-      await user.save(); // Save the updated bank balance
-
-      await bet.save();
-      res.status(200).json({ message: "Bet placed successfully" });
-
+     const winAmount = await callprice(userId, amount, selection, periodId);
+ 
+    //  if (res.periodId === periodId) {
+    //    callprice(userId, amount, selection, periodId);
+    //  } else {
+    //    numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    //    callprice(userId, amount, selection, periodId);
+    //  }
+ 
+     const bet = new Bet({
+       userId,
+       amount,
+       winAmount,
+       // givenAmount,
+       totalAmount,
+       selection,
+       // greenGive,
+       periodId,
+       outcome
+     });
+ 
+     const user = await User.findById(userId);
+     if (!user) {
+       return res.status(404).json({ error: "User not found" });
+     }
+     user.bankBalance -= amount;
+     await user.save(); // Save the updated bank balance
+ 
+     await bet.save();
+     res.status(200).json({ message: "Bet placed successfully" });
    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal server error" });
+     console.error(error);
+     res.status(500).json({ error: "Internal server error" });
    }
-};
+ };
 
 const updateBetOutcome = async (req, res) => {
    try {
@@ -90,14 +108,11 @@ const updateBetOutcome = async (req, res) => {
       // Check if the outcomes have already been updated for this period
       const periodInfo = await PeriodInfo.findOne({ periodId });
       if (periodInfo && periodInfo.outcomeUpdated) {
-         // Outcomes already updated for this period, no need to update again
          return res.status(200).json({ message: "Bet outcomes already updated for this period" });
       }
 
-      // Find all bets for the specified period
       const allBets = await Bet.find({ periodId });
 
-      // Update outcome to 'Win' for winning bets
       const winningBets = allBets.filter(bet => bet.selection === result);
       await Promise.all(winningBets.map(async (bet) => {
          bet.outcome = 'Win';
@@ -105,7 +120,6 @@ const updateBetOutcome = async (req, res) => {
          
       }));
 
-      // Update outcome to 'Loss' for non-winning bets
       const nonWinningBets = allBets.filter(bet => bet.selection !== result);
       await Promise.all(nonWinningBets.map(async (bet) => {
          bet.outcome = 'Loss';
@@ -129,79 +143,86 @@ const updateBetOutcome = async (req, res) => {
 
 const getLowestBetNumber = async (req, res) => {
    try {
-      const periodId = req.params.periodId;
-      console.log("Received Period ID:", periodId);
-
-
-      const betTotals = await Bet.aggregate([
-         { $match: { periodId: parseInt(periodId) } },
-         {
-            $group: {
-               _id: "$selection",
-               totalAmount: { $sum: "$amount" },
-            },
-         },
-         {
-            $project: {
-               _id: 1,
-               totalAmount: 1,
-               multiplier: {
-                  $cond: {
-                     if: {
-                        $or: [
-                           { $in: ["$_id", ["Green", "Red"]] },
-                           { $in: ["$_id", ["1", "2", "3", "4", "6", "7", "8", "9"]] },
-                        ],
-                     },
-                     then: {
-                        $cond: {
-                           if: { $in: ["$_id", ["1", "2", "3", "4", "6", "7", "8", "9"]] },
-                           then: 9, // Multiplier for selections 1, 2, 3, 4, 6, 7, 8, 9
-                           else: 2, // Default multiplier for Green and Red
-                        },
-                     },
-                     else: {
-                        $cond: {
-                           if: { $eq: ["$_id", "Violet"] },
-                           then: 1.4, // Multiplier for Violet
-                           else: 4, // Multiplier for selections 0 and 5
-                        },
-                     },
-                  },
-               },
-            },
-         },
-         { $sort: { totalAmount: 1 } },
-      ]);
-
-      console.log("Bet Totals:", betTotals);
-
-      if (betTotals.length > 0) {
-         const lowestBet = betTotals.find((bet) => bet.multiplier !== null);
-         if (lowestBet) {
-            const multiplyAmount = lowestBet.totalAmount * lowestBet.multiplier;
-            console.log("Lowest Bet:", lowestBet);
-            console.log("Multiply Amount:", multiplyAmount);
-
-            res.status(200).json({
-               lowestBetNumber: lowestBet._id,
-               multiplier: lowestBet.multiplier,
-            });
-         } else {
-            console.log("No valid bets found for this period");
-            res
-               .status(404)
-               .json({ message: "No valid bets found for this period" });
+     const periodId = req.params.periodId;
+     console.log("Received Period ID:", periodId);
+ 
+     // Query to find bets for the specified period ID
+     const bets = await Bet.find({ periodId: parseInt(periodId) }).select(
+       "selection amount"
+     );
+ 
+     console.log("Bets for Period ID:", bets);
+ 
+     if (bets.length > 0) {
+       const betMap = new Map();
+ 
+       // Calculate total amount for each selection
+       bets.forEach((bet) => {
+         const { selection, amount } = bet;
+         const totalAmount = betMap.has(selection)
+           ? betMap.get(selection) + amount
+           : amount;
+         betMap.set(selection, totalAmount);
+       });
+ 
+       // Check if any selection from 0 to 9 is missing
+       const missingSelections = [
+         "0",
+         "1",
+         "2",
+         "3",
+         "4",
+         "5",
+         "6",
+         "7",
+         "8",
+         "9",
+       ].filter((selection) => !betMap.has(selection));
+ 
+       if (missingSelections.length > 0) {
+         // If there are missing selections, choose the lowest missing selection as the lowest bet number
+         const lowestBetNumber = missingSelections.reduce((min, current) =>
+           parseInt(current) < parseInt(min) ? current : min
+         );
+ 
+         console.log("Lowest Bet Number (Missing):", lowestBetNumber);
+ 
+         res.status(200).json({
+           lowestBetNumber: lowestBetNumber,
+           multiplier: 2, // You can adjust the multiplier as needed
+         });
+       } else {
+         // If all selections from 0 to 9 have been bet on, choose the lowest bet based on total amount
+         const sortedBets = [...betMap.entries()].sort((a, b) => a[1] - b[1]);
+         const lowestBet = sortedBets[0];
+ 
+         let multiplier = 2;
+         if (["1", "2", "3", "4", "6", "7", "8", "9"].includes(lowestBet[0])) {
+           multiplier = 9;
+         } else if (lowestBet[0] === "Violet") {
+           multiplier = 1.4;
+         } else if (["0", "5"].includes(lowestBet[0])) {
+           multiplier = 4.5;
          }
-      } else {
-         console.log("No bets found for this period");
-         res.status(404).json({ message: "No bets found for this period" });
-      }
+ 
+         const multiplyAmount = lowestBet[1] * multiplier;
+         console.log("Lowest Bet (Total Amount):", lowestBet);
+         console.log("Multiply Amount:", multiplyAmount);
+ 
+         res.status(200).json({
+           lowestBetNumber: lowestBet[0],
+           multiplier: multiplier,
+         });
+       }
+     } else {
+       console.log("No bets found for this period");
+       res.status(404).json({ message: "No bets found for this period" });
+     }
    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal server error" });
+     console.error("Error:", error);
+     res.status(500).json({ error: "Internal server error" });
    }
-};
+ };
 
 const getAllUserBets = async (req, res) => {
    try {
