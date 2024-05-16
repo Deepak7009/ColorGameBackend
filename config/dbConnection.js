cfa55d9596c6304156382e357bb5a9510de30c2b
vinunarwal <vinunarwal3@gmail.com>
2024-05-16 12:09:42 -0400

    Time updated

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,37 +1,38 @@
 const mongoose = require("mongoose");
-//const {startTime} = require("../controller/timeController");
 const Time = require("../models/timeSchema");
 const moment = require("moment");
 
-
 const conn = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI);
-        console.log("DB Connected", connect.connection.host);
-        let currentTime = await Time.findOne().sort({ _id: -1 }).limit(1);
-        let periodId = 1234567891;
-        if (currentTime) {
-          periodId = currentTime.periodId + 1;
-        }
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB Connected", connect.connection.host);
 
-        if (currentTime) {
-          await Time.updateOne({ _id: currentTime._id }, { isActive: false });
-        }
+    const createNewTime = async () => {
+      let currentTime = await Time.findOne().sort({ _id: -1 }).limit(1);
+      let periodId = currentTime ? currentTime.periodId + 1 : 1;
 
-        const newTime = new Time({
-          periodId,
-          time: 60,
-          startTime: moment(),
-          endTime: moment().add(1, 'minute'),
-          wonNumber: 0, // Update wonNumber based on request body
-        });
-        await newTime.save();
+      if (currentTime) {
+        await Time.updateOne({ _id: currentTime._id }, { isActive: false });
+      }
 
-        console.log("New time created for period ID:", periodId);
-    } catch (err) {
-        console.error("DB connection error:", err);
-        process.exit(1);
-    }
+      const newTime = new Time({
+        periodId,
+        time: 60,
+        startTime: moment(),
+        endTime: moment().add(1, 'minute'),
+        wonNumber: 0,
+      });
+      await newTime.save();
+
+      console.log("New time created for period ID:", periodId);
+      setTimeout(createNewTime, 60000);
+    };
+
+    createNewTime();
+  } catch (err) {
+    console.error("DB connection error:", err);
+    process.exit(1);
+  }
 }
 
 module.exports = conn;
